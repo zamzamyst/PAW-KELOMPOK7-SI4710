d9b801e30a6607dfc6f1b14593e61b57d54b3fb3
zamzamyst <sultan.afzam@gmail.com>
2025-05-30 14:05:36 +0700

    created: setup-manajemen-user-dan-mvc

---
+++ resources/views/delivery/index.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex items-center justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('List Delivery') }}
+            </h2>
+        </div>
+    </x-slot>
+
+</x-app-layout>
