69aee3a5b2529e9910b8e676efb297c34ed47484
zamzamyst <sultan.afzam@gmail.com>
2025-06-01 18:11:21 +0700

    created: api-get-order

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,8 @@ Route::middleware('api')
 Route::middleware('api')
     ->prefix('api')
     ->group(function () {
-        //
+        Route::get('/order', [OrderController::class, 'index']);
+        Route::get('/order/{id}', [OrderController::class, 'show']);
     });
 
 
