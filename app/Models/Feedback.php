482c142684cdbaa442fbcd31d88358cb546a52b1
Sanjaya Fathur Rahman <sanjayafathurrahman@student.telkomuniversity.ac.id>
2025-06-01 12:54:39 +0700

    created: model-feedback

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'feedbacks';
+
+    protected $fillable = [
+        'user_id',
+        'rating',
+        'comment',
+    ];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
