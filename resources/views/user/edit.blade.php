79f59dede3a46c2626f9c4426b238c80ec7386db
madityadjalil <madityadjalil@gmail.com>
2025-06-01 20:19:31 +0700

    Created edit.blade.php

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,8 +1,67 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Edit User') }}
+            {{ __('Edit Order') }}
         </h2>
     </x-slot>
 
+    <div class="py-12">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
+                <h1 class="text-2xl font-bold mb-4">user - {{ $user->id }}</h1>
+                <hr class="mb-6" />
+
+                <form action="{{ route('user.update', $user->id) }}" method="POST" class="space-y-6">
+                    @csrf
+                    @method('PUT')
+
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div>
+                            <label class="block text-gray-700 text-sm font-bold mb-2">Name</label>
+                            <input type="text" name="name" class="form-input w-full" placeholder="Name"
+                                value="{{ old('name', $user->name) }}" >
+                            @error('name')
+                                <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div>
+                            <label class="block text-gray-700 text-sm font-bold mb-2">Email</label>
+                            <input type="email" name="email" class="form-input w-full" placeholder="Email"
+                                value="{{ old('email', $user->email) }}" >
+                            @error('email')
+                                <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div>
+                            <label class="block text-gray-700 text-sm font-bold mb-2">Email Verified</label>
+                            <input type="text" name="email_verified_at" class="form-input w-full" placeholder="Email Verified"
+                                value="{{ old('email_verified_at', $user->email_verified_at) }}" readonly>
+                            @error('email_verified_at')
+                                <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div>
+                            <label class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                            <input type="text" name="password" class="form-input w-full" placeholder="Password"
+                                value="{{ old('password', $user->password) }}">
+                            @error('password')
+                                <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="flex justify-end">
+                        <button type="submit"
+                            class="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold px-6 py-2 rounded">
+                            Update
+                        </button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
 </x-app-layout>
