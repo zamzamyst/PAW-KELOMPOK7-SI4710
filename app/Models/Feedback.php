59efcc6ce910db5fbc974bd34fe904ed4453b9e5
Sanjaya Fathur Rahman <sanjayafathurrahman@student.telkomuniversity.ac.id>
2025-06-01 21:24:09 +0700

    Created: Feedback.php-feedback

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -2,9 +2,43 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'feedbacks';
+
+    protected $fillable = [
+        'user_id',
+        'rating',
+        'comment',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getFormattedRatingAttribute()
+    {
+        return $this->rating . ' / 5';
+    }
+
+    public function getFormattedCommentAttribute()
+    {
+        return $this->comment ? nl2br(e($this->comment)) : 'Tidak ada komentar tersedia :(';
+    }
+
+    public function getCreatedAtFormattedAttribute()
+    {
+        return $this->created_at->format('d M Y, H:i');
+    }
+
+    public function getUpdatedAtFormattedAttribute()
+    {
+        return $this->updated_at->format('d M Y, H:i');
+    }
 }
