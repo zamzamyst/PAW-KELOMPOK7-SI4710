3427eaf0d150a6d76d77417b21c8eac33f1abf8a
Sanjaya Fathur Rahman <sanjayafathurrahman@student.telkomuniversity.ac.id>
2025-06-02 14:15:02 +0700

    Updated: create_feedback_table.php

--- database/migrations/2025_05_30_043335_create_feedback_table.php
+++ database/migrations/2025_05_30_043335_create_feedback_table.php
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('feedbacks', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('order_id')->unique();
             $table->unsignedBigInteger('user_id')->nullable();
             $table->tinyInteger('rating')->comment('Nilai Rating (1-5)');
             $table->text('comment')->nullable();
             $table->timestamps();
 
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
