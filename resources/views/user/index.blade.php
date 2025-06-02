7093177e4eb25ac96ec26ddcd905ab2a0a17783b
zamzamyst <sultan.afzam@gmail.com>
2025-06-02 13:11:31 +0700

    updated: adjusting-views

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex items-center justify-between">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('List Order') }}
+                {{ __('List User') }}
             </h2>
         </div>
     </x-slot>
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Detail user') }}
+            {{ __('Detail User') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
-                <h1 class="text-2xl font-bold mb-2">user - {{ $user->id }}</h1>
+                <h1 class="text-2xl font-bold mb-2">User - {{ $user->id }}</h1>
                 <hr class="mb-6" />
 
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
@@ -23,11 +23,6 @@
                 </div>
 
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-                    <div>
-                        <label class="block text-gray-700 text-sm font-bold mb-2">Email Verified</label>
-                        <input type="text" name="menu_code" class="form-input w-full" value="{{ $user->email_verified_at }}"
-                            readonly>
-                    </div>
                     <div>
                         <label class="block text-gray-700 text-sm font-bold mb-2">Password</label>
                         <input type="text" name="menu_code" class="form-input w-full" value="{{ $user->password }}"
