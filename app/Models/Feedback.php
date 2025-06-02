d7390b851366784548ec8f573ac1b6bef62b90e8
Sanjaya Fathur Rahman <sanjayafathurrahman@student.telkomuniversity.ac.id>
2025-06-02 14:17:23 +0700

    Updated: Feedback.php-feedback

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -12,11 +12,17 @@ class Feedback extends Model
     protected $table = 'feedbacks';
 
     protected $fillable = [
+        'order_id',
         'user_id',
         'rating',
         'comment',
     ];
 
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+    
     public function user()
     {
         return $this->belongsTo(User::class);
